a13e53ca201143f7afbd92aa47f479a52bbc2327
gigabite-hub <king46381@gmail.com>
2024-07-29 22:46:53 +0500

    Swiper breakpoint added

--- custom-script.js
+++ custom-script.js
@@ -21,6 +21,24 @@
                 nextEl: '.navigation-right',
                 prevEl: '.navigation-left',
             },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                },
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 40,
+                }
+            }
         });
 
     });
